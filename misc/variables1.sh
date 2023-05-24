9f4c2314149006c6a7b0badc57c01f3a143e5549
Nageswar5050 <Nageswar0550@gmail.com>
2023-05-24 12:07:10 +0530

    modified variable file

--- misc/variables1.sh
+++ misc/variables1.sh
@@ -2,7 +2,6 @@
 
 name=Nageswar
 company="Kyndryl solutions Pvt Ltd"
-ex=Rajini
 mother="Bhagya Lakshmi"
 father=Ramakrishna
 brother=Kishan
@@ -17,7 +16,4 @@ echo
 echo "My father's name is $father $surname"
 echo
 echo "My brother name is $brother $surname"
-echo
-echo "My Ex-Lover name is $ex"
-echo
 echo
